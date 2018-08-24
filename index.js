75d6aa2c67f7b5abfa63e557b08a384162107093
남현욱 <37534735+hw0k@users.noreply.github.com>
2018-08-24 09:50:15 +0900

    [ADD] emit, once, on, off alias

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import { EventBus } from './event-bus'
 const Vuevent = {
   install(Vue, options) {
     Vue.prototype.$vuevent = EventBus
+    Vue.prototype.$vuevent.emit = Vue.prototype.$vuevent.$emit
+    Vue.prototype.$vuevent.once = Vue.prototype.$vuevent.$once
+    Vue.prototype.$vuevent.on = Vue.prototype.$vuevent.$on
+    Vue.prototype.$vuevent.off = Vue.prototype.$vuevent.$off
   }
 }
 
